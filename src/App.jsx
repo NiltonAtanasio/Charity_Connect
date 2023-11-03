dcc9066bf2fba27c3b2d83b708cbf569e56f22f6
Nilton Atanazzio <nii.cesar@hotmail.com>
2023-11-03 11:25:03 -0300

    first commit ✨

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
