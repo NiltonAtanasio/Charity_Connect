23c52cc4341178e372c7721f679a23c6444e2e0b
Nilton Atanazzio <nii.cesar@hotmail.com>
2023-11-06 21:07:42 -0300

    Nav and Main started 😎

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
+import "./styles/app.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <div className="nav">
+        <div className="nav__logo">
+          <h3>logo</h3>
+        </div>
+        <div className="nav__menu">
+          <h3>Home</h3>
+          <h3>About</h3>
+          <h3>FAQs</h3>
+          <h3>Login</h3>
+        </div>
+      </div>
+
+      <div className="main">
+        <div className="main__text">
+          <h1>
+            O grupo CharityConnect ajuda as pessoas a se conectarem, encontrarem
+            comunidades e expandirem seu alcance
+          </h1>
+          <button>Cadastrar</button>
+        </div>
+        <div className="main__image"></div>
+      </div>
+
+      <div className="titles"></div>
     </div>
   );
 }
