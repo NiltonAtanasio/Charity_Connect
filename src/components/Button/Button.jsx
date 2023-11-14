3b068a3e8dfbdd60ddb75953e72f29e06514d634
Nilton Atanazzio <nii.cesar@hotmail.com>
2023-11-14 20:42:52 -0300

    Button link 🔗

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Button.css";
 
 export default function Button(props) {
-  return <button>{props.text}</button>;
+  return (
+    <button>
+      <Link to={`/${props.link}`}>{props.text}</Link>
+    </button>
+  );
 }
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
             O grupo CharityConnect ajuda as pessoas a se conectarem, encontrarem
             comunidades e expandirem seu alcance
           </h1>
-          <Button text="Cadastrar" />
+          <Button text="Cadastrar" link="cadastro" />
         </div>
         <div className="main__image">
           <img src={logo} alt="" />
